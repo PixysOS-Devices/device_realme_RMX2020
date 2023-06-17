3bb574a5fe1cc0d467f846c2ba00e2a65c46d71e
Koushik Dey <kousickdey3@gmail.com>
2023-06-18 00:32:27 +0530

    RMX2020: PixysOS Bringup
    Signed-off-by: Koushik Dey <kousickdey3@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_RMX2020.mk
+    $(LOCAL_DIR)/pixys_RMX2020.mk
 
 COMMON_LUNCH_CHOICES := \
-    arrow_RMX2020-user \
-    arrow_RMX2020-userdebug \
-    arrow_RMX2020-eng
+    pixys_RMX2020-user \
+    pixys_RMX2020-userdebug \
+    pixys_RMX2020-eng
